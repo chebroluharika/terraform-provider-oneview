ff18cacac0466860ae638a8cb5707a336e4444da
Nabhajit Ray <nabhajit.ray@hpe.com>
2020-05-02 00:38:52 -0400

    resource_file_changed

---
+++ hypervisor_cluster_profile.tf
@@ -0,0 +1,37 @@
+provider "oneview" {
+	ov_username = "Administrator"
+	ov_password = "admin123"
+	ov_endpoint = "https://10.50.9.31"
+	ov_sslverify = false
+	ov_apiversion = 1200
+	ov_ifmatch = "*"
+}
+
+data "oneview_scope" "scope_obj" {
+        name = "test_scope"
+}
+
+
+resource "oneview_hypervisor_cluster_profile" "HypervisorClusterProfile"{  
+    "type"="HypervisorClusterProfileV3",
+    "name"="Cluster5",
+    "description"="",
+    "hypervisor_type"="Vmware",
+    "hypervisor_manager_uri"="/rest/hypervisor-managers/444f88c8-3a66-4f20-9b87-0c1da9542df8",
+    "path"="DC2",
+    "hypervisor_cluster_settings"={  
+                                  "type"="Vmware",
+                                  "drs_enabled"=true,
+                                  "ha_enabled"=false,
+                                  "multi_nic_v_motion"=false,
+                                  "virtual_switch_type"="Standard"
+                               },
+    "hypervisor_host_profile_template"={  
+    "server_profile_template_uri"="/rest/server-profile-templates/278cadfb-2e86-4a05-8932-972553518259"
+         "deployment_plan"={  
+         "deployment_plan_uri"="/rest/os-deployment-plans/3abe82af-adcc-44bc-8af5-0a25593283f8"
+         "server_password"="myPassword"
+         },
+     "host_prefix"="Test-Cluster-host"
+     }
+}
--- oneview/resource_hypervisor_cluster_profile.go
+++ oneview/resource_hypervisor_cluster_profile.go
@@ -15,6 +15,9 @@ import (
 	"github.com/HewlettPackard/oneview-golang/ov"
 	"github.com/HewlettPackard/oneview-golang/utils"
 	"github.com/hashicorp/terraform/helper/schema"
+	"path"
+	"encoding/json"
+	"io/ioutil"
 )
 
 func resourceHypervisorClusterProfile() *schema.Resource {
@@ -24,7 +27,7 @@ func resourceHypervisorClusterProfile() *schema.Resource {
 		Schema: map[string]*schema.Schema{
 			"add_host_requests": {
 				Type:     schema.TypeSet,
-				Computed: true,
+				Optional: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString},
 				Set: schema.HashString,
@@ -32,196 +35,196 @@ func resourceHypervisorClusterProfile() *schema.Resource {
 
 			"category": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"compliance_state": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"created": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"description": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"e_tag": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"hypervisor_cluster_settings": {
 				Type:     schema.TypeSet,
-				Computed: true,
+				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 
 						"distributed_switch_usage": {
 							Type:     schema.TypeString,
-							Computed: true,
+							Optional: true,
 						},
 						"distributed_switch_version": {
 							Type:     schema.TypeString,
-							Computed: true,
+							Optional: true,
 						},
 						"drs_enabled": {
 							Type:     schema.TypeBool,
-							Computed: true,
+							Optional: true,
 						},
 						"ha_enabled": {
 							Type:     schema.TypeBool,
-							Computed: true,
+							Optional: true,
 						},
 
 						"multi_nic_v_motion": {
 							Type:     schema.TypeBool,
-							Computed: true,
+							Optional: true,
 						},
 
 						"type": {
 							Type:     schema.TypeString,
-							Computed: true,
+							Optional: true,
 						},
 
 						"virtual_switch_type": {
 							Type:     schema.TypeString,
-							Computed: true,
+							Optional: true,
 						},
 					},
 				},
 			},
 			"hypervisor_host_profile_template": {
 				Type:     schema.TypeSet,
-				Computed: true,
+				Optional: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"deployment_manager_type": {
 							Type:     schema.TypeString,
-							Computed: true},
+							Optional: true},
 						"deployment_plan": {
 							Type:     schema.TypeList,
-							Computed: true,
+							Optional: true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"deployment_custom_args": {
 										Type:     schema.TypeList,
-										Computed: true,
+										Optional: true,
 										Elem: &schema.Schema{
 											Type: schema.TypeString,
 										},
 									},
 									"deployment_plan_description": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"deployment_plan_uri": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"name": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"server_password": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 								}}},
 						"host_config_policy": {
 							Type:     schema.TypeList,
-							Computed: true,
+							Optional: true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"leave_host_in_maintenance": {
 										Type:     schema.TypeBool,
-										Computed: true},
+										Optional: true},
 									"use_host_prefix_as_hostname": {
 										Type:     schema.TypeBool,
-										Computed: true},
+										Optional: true},
 									"use_hostname_to_register": {
 										Type:     schema.TypeBool,
-										Computed: true},
+										Optional: true},
 								}}},
 						"host_prefix": {
 							Type:     schema.TypeString,
-							Computed: true},
+							Optional: true},
 						"server_profile_template_uri": {
 							Type:     schema.TypeString,
-							Computed: true},
+							Optional: true},
 						"virtual_switch_config_policy": {
 							Type:     schema.TypeList,
-							Computed: true,
+							Optional: true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 
 									"configure_port_group": {
 										Type:     schema.TypeBool,
-										Computed: true},
+										Optional: true},
 									"custom_virtual_switches": {
 										Type:     schema.TypeBool,
-										Computed: true},
+										Optional: true},
 									"manage_virtual_switches": {
 										Type:     schema.TypeBool,
-										Computed: true},
+										Optional: true},
 								}}},
 						"virtual_switches": {
 							Type:     schema.TypeList,
-							Computed: true,
+							Optional: true,
 							Elem: &schema.Resource{
 								Schema: map[string]*schema.Schema{
 									"action": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"name": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"network_uris": {
 										Type:     schema.TypeList,
-										Computed: true,
+										Optional: true,
 										Elem: &schema.Schema{
 											Type: schema.TypeString},
 									},
 									"version": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"virtual_switch_port_groups": {
 										Type:     schema.TypeList,
-										Computed: true,
+										Optional: true,
 										Elem: &schema.Resource{
 											Schema: map[string]*schema.Schema{
 												"action": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 												"name": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 												"network_uris": {
 													Type:     schema.TypeList,
-													Computed: true,
+													Optional: true,
 													Elem: &schema.Schema{
 														Type: schema.TypeString},
 												},
 												"virtual_switch_ports": {
 													Type:     schema.TypeList,
-													Computed: true,
+													Optional: true,
 													Elem: &schema.Resource{
 														Schema: map[string]*schema.Schema{
 															"action": {
 																Type:     schema.TypeString,
-																Computed: true},
+																Optional: true},
 															"dhcp": {
 																Type:     schema.TypeBool,
-																Computed: true},
+																Optional: true},
 															"ip_address": {
 																Type:     schema.TypeString,
-																Computed: true},
+																Optional: true},
 															"subnet_mask": {
 																Type:     schema.TypeString,
-																Computed: true},
+																Optional: true},
 															"virtual_port_purpose": {
 																Type:     schema.TypeList,
-																Computed: true,
+																Optional: true,
 																Elem: &schema.Schema{
 																	Type: schema.TypeString},
 															},
@@ -229,32 +232,32 @@ func resourceHypervisorClusterProfile() *schema.Resource {
 													}},
 												"vlan": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 											}},
 									},
 									"virtual_switch_type": {
 										Type:     schema.TypeString,
-										Computed: true},
+										Optional: true},
 									"virtual_switch_uplinks": {
 										Type:     schema.TypeList,
-										Computed: true,
+										Optional: true,
 										Elem: &schema.Resource{
 											Schema: map[string]*schema.Schema{
 												"action": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 												"active": {
 													Type:     schema.TypeBool,
-													Computed: true},
+													Optional: true},
 												"mac": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 												"name": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 												"vmnic": {
 													Type:     schema.TypeString,
-													Computed: true},
+													Optional: true},
 											},
 										},
 									},
@@ -263,27 +266,27 @@ func resourceHypervisorClusterProfile() *schema.Resource {
 					}}},
 			"hypervisor_cluster_uri": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"hypervisor_host_profile_uris": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"hypervisor_manager_uri": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"hypervisor_type": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"ip_pools": {
 				Type:     schema.TypeSet,
-				Computed: true,
+				Optional: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString},
 				Set: schema.HashString,
@@ -291,37 +294,37 @@ func resourceHypervisorClusterProfile() *schema.Resource {
 
 			"mgmt_ip_settings_override": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"modified": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"name": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"path": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"refresh_state": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"scopes_uri": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"shared_storage_volumes": {
 				Type:     schema.TypeSet,
-				Computed: true,
+				Optional: true,
 				Elem: &schema.Schema{
 					Type: schema.TypeString},
 				Set: schema.HashString,
@@ -329,22 +332,22 @@ func resourceHypervisorClusterProfile() *schema.Resource {
 
 			"state": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"state_reason": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"status": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"type": {
 				Type:     schema.TypeString,
-				Computed: true,
+				Optional: true,
 			},
 
 			"uri": {
@@ -359,31 +362,31 @@ func resourceHypervisorClusterProfileCreate(d *schema.ResourceData, meta interfa
 	config := meta.(*Config)
 
 	hypCP := ov.HypervisorClusterProfile{
-		AddHostRequests: d.Get("add_host_requests").(string),
+		//AddHostRequests: d.Get("add_host_requests").(string),
 		Category:        d.Get("category").(string),
 		ComplianceState: d.Get("compliance_state").(string),
 		Created:         d.Get("created").(string),
-		Description:     d.Get("description").(string),
+		Description:     utils.Nstring(d.Get("description").(string)),
 		ETag:            d.Get("e_tag").(string),
 		//HypervisorClusterSettings:     d.Get("hypervisor_cluster_settings").(string),
 		HypervisorClusterUri: d.Get("hypervisor_cluster_uri").(string),
 		//HypervisorHostProfileTemplate: d.Get("hypervisor_host_profile_template").(string),
-		HypervisorHostProfileUris: d.Get("hypervisor_host_profile_uris").(string),
-		HypervisorManagerUri:      d.Get("hypervisor_manager_uri").(string),
+		HypervisorHostProfileUris: utils.Nstring(d.Get("hypervisor_host_profile_uris").(string)),
+		HypervisorManagerUri:      utils.Nstring(d.Get("hypervisor_manager_uri").(string)),
 		HypervisorType:            d.Get("hypervisor_type").(string),
-		IpPools:                   d.Get("ip_pools").(string),
-		MgmtIpSettingsOverride:    d.Get("mgmt_ip_settings_override").(string),
-		Modified:                  d.Get("modified").(string),
-		Name:                      d.Get("name").(string),
-		Path:                      d.Get("path").(string),
-		RefreshState:              d.Get("refresh_state").(string),
-		ScopesUri:                 d.Get("scopes_uri").(string),
-		SharedStorageVolumes:      d.Get("shared_storage_volumes").(string),
-		State:                     d.Get("state").(string),
-		StateReason:               d.Get("state_reason").(string),
-		Status:                    d.Get("status").(string),
-		Type:                      d.Get("type").(string),
-		Uri:                       d.Get("uri").(string),
+		//IpPools:                   utils.Nstring(d.Get("ip_pools").(string)),
+		MgmtIpSettingsOverride: d.Get("mgmt_ip_settings_override").(string),
+		Modified:               d.Get("modified").(string),
+		Name:                   d.Get("name").(string),
+		Path:                   d.Get("path").(string),
+		RefreshState:           d.Get("refresh_state").(string),
+		ScopesUri:              d.Get("scopes_uri").(string),
+		//SharedStorageVolumes:      utils.Nstring(d.Get("shared_storage_volumes").(string)),
+		State:       d.Get("state").(string),
+		StateReason: d.Get("state_reason").(string),
+		Status:      d.Get("status").(string),
+		Type:        d.Get("type").(string),
+		URI:         utils.Nstring(d.Get("uri").(string)),
 	}
 	HypervisorClusterSettingslist := d.Get("hypervisor_cluster_settings").(*schema.Set).List()
 	for _, raw := range HypervisorClusterSettingslist {
@@ -391,10 +394,10 @@ func resourceHypervisorClusterProfileCreate(d *schema.ResourceData, meta interfa
 
 		hypClusterSettings := ov.HypervisorClusterSettings{
 			DistributedSwitchUsage:   hypervisorClusterSettings["distributed_switch_usage"].(string),
-			DistributedSwitchVersion: hypervisorClusterSettings["distributed_switch_version"].(bool),
+			DistributedSwitchVersion: hypervisorClusterSettings["distributed_switch_version"].(string),
 			DrsEnabled:               hypervisorClusterSettings["drs_enabled"].(bool),
-			HaEnabled:                hypervisorClusterSettings["ha_enabled"].(string),
-			MultiNicVMotion:          hypervisorClusterSettings["multi_nic_v_motion"].(string),
+			HaEnabled:                hypervisorClusterSettings["ha_enabled"].(bool),
+			MultiNicVMotion:          hypervisorClusterSettings["multi_nic_v_motion"].(bool),
 			Type:                     hypervisorClusterSettings["type"].(string),
 			VirtualSwitchType:        hypervisorClusterSettings["virtual_switch_type"].(string),
 		}
@@ -402,11 +405,25 @@ func resourceHypervisorClusterProfileCreate(d *schema.ResourceData, meta interfa
 	}
 	HypervisorHostProfileTemplateList := d.Get("hypervisor_host_profile_template").(*schema.Set).List()
 	for _, raw := range HypervisorHostProfileTemplateList {
-		deploymentplanlist := HypervisorHostProfileTemplateList.Get("deployment_plan").(*schema.Set).List()
+		/******************* deployment plan start********************/
+		var hptdeploymentplan ov.DeploymentPlan
+		var dpCustomArgs  []utils.Nstring
+
+		dp_map, _ := (d.Get("hypervisor_host_profile_template")).(map[string]interface{})
+		deploymentplanlist := dp_map["deployment_plan"].(*schema.Set).List()
 		for _, dp_raw := range deploymentplanlist {
-			deploymentPlan := raw(map[string]interface{})
-			hptdeploymentplan := HypervisorHostProfileTemplateList.DeploymentPlan{
-				DeploymentCustomArgs:      deploymentPlan["deployment_custom_args"].(string),
+			deploymentPlan := dp_raw.(map[string]interface{})
+			/*******************dp_custom-args start***********************/
+			if val, ok := deploymentPlan["deployment_custom_args"]; ok {
+				dpCustomArgsOrder := val.(*schema.Set).List()
+				dpCustomArgs = make([]utils.Nstring, len(dpCustomArgsOrder))
+				for i, raw := range dpCustomArgsOrder {
+					dpCustomArgs[i] = utils.Nstring(raw.(string))
+				}
+			}
+			/********************dp custom args end**********************/
+			hptdeploymentplan = ov.DeploymentPlan{
+				DeploymentCustomArgs:      dpCustomArgs,
 				DeploymentPlanDescription: deploymentPlan["deployment_plan_description"].(string),
 				DeploymentPlanUri:         utils.Nstring(deploymentPlan["deployment_plan_uri"].(string)),
 				Name:                      deploymentPlan["name"].(string),
@@ -414,7 +431,10 @@ func resourceHypervisorClusterProfileCreate(d *schema.ResourceData, meta interfa
 			}
 
 		}
+		file, _ := json.MarshalIndent(hptdeploymentplan, "", " ")
+		_ = ioutil.WriteFile("dp.json", file, 0644)
 
+		/********************deployment plan end**********************************************/
 		hostprofiletemplate := raw.(map[string]interface{})
 		hypHostProfileTemplate := ov.HypervisorHostProfileTemplate{
 			DeploymentManagerType:    hostprofiletemplate["deployment_manager_type"].(string),
@@ -422,10 +442,13 @@ func resourceHypervisorClusterProfileCreate(d *schema.ResourceData, meta interfa
 			Hostprefix:               hostprofiletemplate["host_prefix"].(string),
 			ServerProfileTemplateUri: utils.Nstring(hostprofiletemplate["server_profile_template_uri"].(string)),
 		}
-		servC.HypervisorHostProfileTemplate = &hypHostProfileTemplate
+		hypCP.HypervisorHostProfileTemplate = &hypHostProfileTemplate
 	}
+	/**********************hypervisor hosr profile end************************************************/
 	hypCPError := config.ovClient.CreateHypervisorClusterProfile(hypCP)
-	d.SetId(d.Get("name").(string))
+	uri := d.Get("URI").(string)
+	_, id := path.Split(uri)
+        d.SetId(id)
 	if hypCPError != nil {
 		d.SetId("")
 		return hypCPError
@@ -434,9 +457,10 @@ func resourceHypervisorClusterProfileCreate(d *schema.ResourceData, meta interfa
 }
 
 func resourceHypervisorClusterProfileRead(d *schema.ResourceData, meta interface{}) error {
-
+	return nil
 }
-func resourceHypervisorClusterProfileUpdate(d *schema.ResourceData, meta interface{}) error {
+
+/*func resourceHypervisorClusterProfileUpdate(d *schema.ResourceData, meta interface{}) error {%
 	config := meta.(*Config)
 
 	hypCP := ov.HypervisorClusterProfile{
@@ -456,12 +480,12 @@ func resourceHypervisorClusterProfileUpdate(d *schema.ResourceData, meta interfa
 	d.SetId(d.Get("name").(string))
 
 	return resourceHypervisorClusterProfileRead(d, meta)
-}
+}*/
 
 func resourceHypervisorClusterProfileDelete(d *schema.ResourceData, meta interface{}) error {
 	config := meta.(*Config)
 
-	err := config.ovClient.DeleteHypervisorClusterProfile(d.Get("name").(string))
+	err := config.ovClient.DeleteHypervisorClusterProfile(d.Id())
 	if err != nil {
 		return err
 	}
